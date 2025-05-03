0179e3f2c407e3c37eba8adb1b2faf4a30940c00
Cezar Silva <cezarfelipe2008@outlook.com>
2025-05-03 04:29:25 -0300

    feat: criar contract firts (openapi/swagger).

---
+++ app/src/main/java/br/brazona/bzn_gdr_services/BznGdrServicesApplication.java
@@ -0,0 +1,13 @@
+package br.brazona.bzn_gdr_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BznGdrServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BznGdrServicesApplication.class, args);
+	}
+
+}
