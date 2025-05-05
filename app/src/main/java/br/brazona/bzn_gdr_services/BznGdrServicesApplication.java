d955cdc719faeb9e831d0bc09460f16c58479a6a
Cezar Silva <cezarfelipe2008@outlook.com>
2025-05-05 18:46:51 -0300

    test: api gateway openapi 3.0

--- app/src/main/java/br/brazona/bzn_gdr_services/BznGdrServicesApplication.java
+++ app/src/main/java/br/brazona/bzn_gdr_services/BznGdrServicesApplication.java
@@ -2,7 +2,9 @@ package br.brazona.bzn_gdr_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BznGdrServicesApplication {
 
